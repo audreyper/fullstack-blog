3177c32b42a974a1ec68e74fb28f25ea1140191b
Audrey <audreyper@protonmail.com>
2025-02-26 13:34:28 -0800

    Improving Navbar and post cards UI

--- frontend/blog-frontend/src/components/Navbar.js
+++ frontend/blog-frontend/src/components/Navbar.js
@@ -1,17 +1,14 @@
 'use client'
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from '../styles/Navbar.module.css'; // Import CSS for styling
+import styles from '../styles/Navbar.module.css'; 
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      {/* Logo on the left */}
-      <div className={styles.logo}>
+      <Link href="/" className={styles.logo}>
         <Image src="/logo.png" alt="Logo" width={100} height={50} priority />
-      </div>
-
-      {/* Navigation Links */}
+      </Link>
       <div className={styles.navLinks}>
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
