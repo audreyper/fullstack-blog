c27b4ade3cad3c9c6d6708cec9aa7d1608be784e
Audrey <audreyper@protonmail.com>
2025-03-11 18:41:02 -0700

    adding code and list sections to render properly in each post

--- frontend/blog-frontend/src/app/posts/[id]/page.js
+++ frontend/blog-frontend/src/app/posts/[id]/page.js
@@ -8,19 +8,20 @@ import Footer from "@/components/Footer";
 import styles from "./PostPage.module.css"; 
 
 const PostPage = () => {
- 
+ // Extract the post ID from the URL parameters
   const { id } = useParams(); 
   const [post, setPost] = useState(null);
 
+// Fetch post data from backend API 
   useEffect(() => {
     if (id) {
       axios
         .get(`http://127.0.0.1:8000/api/posts/${id}/`)
-        .then((response) => setPost(response.data))
-        .catch((error) => console.error("Error fetching post:", error));
+        .then((response) => setPost(response.data)) // Store fetched post data
+        .catch((error) => console.error("Error fetching post:", error)); // Log any errors
     }
   }, [id]);
-
+  // If the post data isn't available yet, show a loading message
   if (!post) return <p>Loading...</p>;
 
   const { content } = post; 
@@ -35,6 +36,7 @@ const PostPage = () => {
         className={styles.image} 
       />
       <div>
+        {/* Loop through each section in the post's content and render based on section type */}
         {content &&
           content.map((section, index) => {
             return (
@@ -49,6 +51,18 @@ const PostPage = () => {
                   <div className={styles.quote}> 
                     <blockquote>{section.content}</blockquote>
                   </div>
+                )}
+                {section.type === "code" && (
+                  <pre className={styles.codeBlock}> 
+                    <code>{section.content}</code>
+                  </pre>
+                )}
+                     {section.type === "list" && (
+                  <ul className={styles.list}> 
+                    {section.content.map((item, i) => (
+                      <li key={i}>{item}</li>
+                    ))}
+                  </ul>
                 )}
               </div>
             );
