feb900013bd046ca6b466f695d6254d54682b843
Audrey <audreyper@protonmail.com>
2025-03-18 17:02:37 -0700

    Fixed rendering issues in postcard and better formatted HomePage css

--- frontend/blog-frontend/src/components/PostCard.js
+++ frontend/blog-frontend/src/components/PostCard.js
@@ -4,14 +4,17 @@ import Link from 'next/link';
 import styles from '../styles/PostCard.module.css';
 
 const PostCard = ({ post }) => {
-  const MAX_CHARACTERS = 116; // Set a reasonable limit
   const firstContent = post.content?.[0]?.content ?? "No content available";
 
   // Ensure content is trimmed but maintains a stable layout
-  const trimmedContent =
-    firstContent.length > MAX_CHARACTERS
-      ? firstContent.substring(0, MAX_CHARACTERS) + "..."
-      : firstContent;
+  let trimmedContent = []; 
+  if (post.title.length < 30) {
+    trimmedContent = firstContent.substring(0, 116) + "..."
+    } else if (post.title.length > 30 && post.title.length < 50) {
+    trimmedContent = firstContent.substring(0, 40) + "..."
+    } else if (post.title.length > 50) {
+    trimmedContent = []
+    }
 
   return (
     <Link href={`/posts/${post.id}`} className={styles.cardLink}>
